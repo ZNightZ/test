cae7ca2d601b7f5398d9f1ce40dd81a2770412d0
Night <rolyalnight@gmail.com>
2020-04-02 04:25:37 +0700

    this

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.post("/login", function (req, res) {
             console.log("-------------------------------");
         }
         else {
-            res.sendFile(__dirname + "/views/course.html");
+            res.end(__dirname + "/course.html");
             console.log("Username: " + result[0].username);
             console.log("-------------------------------");
         }
