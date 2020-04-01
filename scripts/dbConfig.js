7da63063714865af133cea9d7e423a50dce56d97
Night <rolyalnight@gmail.com>
2020-04-01 21:31:43 +0700

    first commit

---
+++ scripts/dbConfig.js
@@ -0,0 +1,15 @@
+// let config = {
+//     host: 'remotemysql.com',
+//     user: 'eWgSc039wD',
+//     password: 'PlXvLos94Q',
+//     database: 'eWgSc039wD'
+// }
+
+let config = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'peguard'
+}
+
+module.exports = config;
